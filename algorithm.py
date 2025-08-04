0572bd5d5ea3d4f4a5bab1204efa866446510b59
Baloch-Rasheed <balochrasheed888@gmail.com>
2025-08-05 01:51:03 +0500

    A-start algorithm is added

--- algorithm.py
+++ algorithm.py
@@ -78,3 +78,45 @@ def gbfs(maze, start, end):
                     frontier.put((priority, next_pos))
                     came_from[next_pos] = current
     return None
+
+# A* | A-star algorithm to find a path in the maze.
+def a_star(maze, start, end):
+    from queue import PriorityQueue
+
+    def heuristic(a, b):
+        return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+    frontier = PriorityQueue()
+    frontier.put((0, start))
+    visited = {start}
+    g_score = {start: 0}
+    h_score = {start: heuristic(start, end)}
+    came_from = {start: None}
+
+    while not frontier.empty():
+        current = frontier.get()[1]
+
+        if current == end:
+            path = []
+            while current is not None:
+                path.append(current)
+                current = came_from[current]
+            return path[::-1]
+
+        for y, x in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            next_y, next_x = current[0] + y, current[1] + x
+            next_pos = (next_y, next_x)
+
+            if 0 <= next_y < len(maze) and 0 <= next_x < len(maze[0]):
+                if maze[next_y][next_x] != '#':
+                    tentative_g_score = g_score[current] + 1
+
+                    if next_pos not in g_score or tentative_g_score < g_score[next_pos]:
+                        came_from[next_pos] = current
+                        g_score[next_pos] = tentative_g_score
+                        h_score[next_pos] = tentative_g_score + heuristic(next_pos, end)
+
+                        if next_pos not in visited:
+                            visited.add(next_pos)
+                            frontier.put((h_score[next_pos], next_pos))
+    return None
